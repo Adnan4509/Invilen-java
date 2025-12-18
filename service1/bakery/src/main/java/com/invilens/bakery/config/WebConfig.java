67031b420e987cf5a390616315961fae50a88420
Adnan4509 <adnanwebdev7@gmail.com>
2025-12-18 20:04:35 +0530

    implemented basic logging interceptor

---
+++ service1/bakery/src/main/java/com/invilens/bakery/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.invilens.bakery.config;
+
+import com.invilens.bakery.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor())
+                .addPathPatterns("/api/**");
+    }
+}
---
+++ service1/bakery/src/main/java/com/invilens/bakery/interceptor/LoggingInterceptor.java
@@ -0,0 +1,28 @@
+package com.invilens.bakery.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class LoggingInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("THE PREHANDLE REQUEST IS :" + request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("THE POSTHANDLE REQUEST IS :" + request.getRequestURI());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("AFTER COMPLETION THE REQUEST IS :" + request.getRequestURI());
+    }
+
+}

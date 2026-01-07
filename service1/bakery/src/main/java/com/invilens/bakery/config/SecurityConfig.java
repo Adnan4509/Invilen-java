551e88fb01e45ad8188bf893971bfc66a75222cb
Adnan4509 <adnanwebdev7@gmail.com>
2026-01-07 11:16:18 +0530

    refactored code

--- service1/bakery/src/main/java/com/invilens/bakery/config/SecurityConfig.java
+++ service1/bakery/src/main/java/com/invilens/bakery/config/SecurityConfig.java
@@ -1,24 +1,17 @@
 package com.invilens.bakery.config;
 
-import com.invilens.bakery.security.KeycloakRoleConverter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
-//@RequiredArgsConstructor
 public class SecurityConfig {
-
-//    public final KeycloakRoleConverter roleConverter;
 
 
     @Bean
@@ -27,14 +20,8 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests()
-//                        auth -> auth
-//                        .requestMatchers(HttpMethod.POST,"/api/bakery").hasRole("ADMIN")
-                        .anyRequest()
-                .authenticated();
-//                )
-//                .oauth2ResourceServer(oauth ->
-//                        oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter()))
-//                );
+                  .anyRequest()
+                    .authenticated();
         httpSecurity
                 .oauth2ResourceServer()
                 .jwt();
@@ -45,11 +32,5 @@ public class SecurityConfig {
 
 
     }
-//
-//    @Bean
-//    public JwtAuthenticationConverter jwtAuthConverter() {
-//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-//        converter.setJwtGrantedAuthoritiesConverter(roleConverter);
-//        return converter;
-//    }
+
 }
